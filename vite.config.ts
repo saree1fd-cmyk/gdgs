985bee009bf6d07d420afa39b60e03eb10511524
aymenmohammed22 <aimenmo123@gmail.com>
2025-09-19 03:58:48 +0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -44,10 +44,10 @@ export default defineConfig(async () => {
     },
     server: {
       host: "0.0.0.0",
-      port: 10000,
+      port: 5000,
       strictPort: false,
       hmr: {
-        port: 10000,
+        port: 5000,
       },
       fs: {
         strict: false,
